48412e9d69f2f295c2db0b2214cc243b958d0094
KpuBemKa <kakoitopis@gmail.com>
2024-08-06 23:05:13 +0300

    idk

--- test.py
+++ test.py
@@ -34,31 +34,102 @@ def efficient_sleep(secs: float):  # for longer times
     return
 
 
-def calc_time_to_reach_block(distance: int) -> float:
+def get_velocity_next_tick(current_tick_speed: float) -> float:
+    SLIPPERINESS_MULTIPLIER = 1.0  # 1.0 for airborne
+    MOVEMENT_MULTIPLIER = (
+        1.0 * 0.98
+    )  # 1.0 for walking (not sprinting); 0.98 facing completely forward
+
+    return (
+        current_tick_speed * SLIPPERINESS_MULTIPLIER * 0.91 + 0.02 * MOVEMENT_MULTIPLIER
+    )
+
+
+def calc_time_to_reach_block(travel_distance: int) -> float:
+    match travel_distance:
+        case 1:
+            return 0.17
+
+        case 2:
+            return 0.35
+        
+        case 3:
+            return 0.5
+        
+        case 4:
+            return 0.6
+        
+        case 5:
+            return 0.7
+        
+        case 6:
+            return 0.825
+        
+    
+    # # FLY_FRICTION = 0.05  # ice friction which is the same as flying
+    # FLY_FRICTION = 0.546  # ice friction which is the same as flying
+    # # ACCEL = 0.53
+    # ACCEL = 0.098
+
+    # distance = 0
+    # velocity = 0
+    # tick_counter = 0
+    # while distance < travel_distance:
+    #     velocity += ACCEL
+
+    #     print(velocity)
+    #     distance += velocity
+
+    #     velocity = velocity * FLY_FRICTION
+
+    #     tick_counter += 1
+
+    # return tick_counter / 20
+
     # one minecraft tick is 1/20 of a second
-    ACCEL_DECAY = 0.003  # blocks/tick^3
-    INITIAL_ACCEL = 0.05  # blocks/tick^2
+    # ACCEL_DECAY = 0.003  # blocks/tick^3
+    # INITIAL_ACCEL = 0.05  # blocks/tick^2
+    # ACCEL_DECAY = 0.0025  # blocks/tick^3
+    # ACCEL_DECAY = 0  # blocks/tick^3
+    # INITIAL_ACCEL = 0.08  # blocks/tick^2
 
-    accel = INITIAL_ACCEL
-    speed = 0
-    tick_counter = 0
+    # accel = INITIAL_ACCEL
+    # speed = 0
+    # tick_counter = 0
 
-    while distance > 0:
-        speed += accel
-            
-        if accel >= 0:
-            accel -= ACCEL_DECAY
+    # while distance > 0:
+    #     speed += accel
 
-        print(speed)
-        distance -= speed
-        tick_counter += 1
+    #     if accel > 0:
+    #         accel -= ACCEL_DECAY
+    #     else:
+    #         accel = 0
+    #     # accel -= ACCEL_DECAY
 
-    return tick_counter / 20
+    #     # print(accel)
+
+    #     # print(speed)
+    #     distance -= speed
+    #     tick_counter += 1
+
+    # print(speed)
+    # return tick_counter / 20.0
+    # distance = 0.0
+    # speed = 0.0
+    # tick_counter = 0
+    # while distance < travel_distance:
+    #     speed = get_velocity_next_tick(speed)
+    #     print(speed)
+    #     distance += speed
+    #     tick_counter += 1
+
+    # print(speed)
+    # return tick_counter / 20
 
 
 def fly_blocks(amount: int) -> None:
     time_to_sleep = calc_time_to_reach_block(amount)
-    print(f"Time to sleep: {time_to_sleep}")
+    print(time_to_sleep)
 
     type_command("home")
 
@@ -68,9 +139,9 @@ def fly_blocks(amount: int) -> None:
 
     efficient_sleep(time_to_sleep)
 
-    keyboard.release("w")
-
     slow_keyboard_tap(Key.shift_l)
+
+    keyboard.release("w")
 
     time.sleep(2)
 
@@ -79,17 +150,38 @@ def fly_blocks(amount: int) -> None:
     slow_keyboard_tap(Key.space)
 
 
-def start_test(blocks: int):
+def execute_cycle(cycle_len: int) -> None:
+    type_command("sethome")
+    
+    for it in range(1, cycle_len + 1):
+        fly_blocks()
+
+
+def start_test():
+    for i in range(1, 6):
+        print(f"Testing for: {i}")
+        for _ in range(0, 3):
+            # fly_blocks(10 * i)
+            fly_blocks(i)
+
+
+def test_specific(blocks):
     while True:
         fly_blocks(blocks)
 
 
 def main():
-    line_length = int(sys.argv[1])
+    if len(sys.argv) == 2:
+        line_length = int(sys.argv[1])
+    else:
+        line_length = None
 
     def key_press_listener(key: KeyCode | Key):
         if key == Key.f8:
-            start_test(line_length)
+            if line_length:
+                test_specific(line_length)
+            else:
+                start_test()
 
     listener = Listener(on_press=key_press_listener)
     listener.start()
@@ -99,3 +191,7 @@ def main():
 
 
 main()
+
+# for i in range(1, 6):
+#     calc_time_to_reach_block(i)
+#     print("------")
